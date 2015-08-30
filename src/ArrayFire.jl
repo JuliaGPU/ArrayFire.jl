28f809bb1b11ef88828f2f9aab32a875610e795d
Keno Fischer <keno@juliacomputing.com>
2015-08-30 10:18:55 +0000

    use the correct function for 2D FFT
    In Julia, the fft function calls a 2D FFT on a 2D Array by default, whereas in ArrayFire, fft performs a column-wise FFT. The appropriate function call in ArrayFire for a 2D FFT is fft2.

--- src/ArrayFire.jl
+++ src/ArrayFire.jl
@@ -372,8 +372,21 @@ chol!(a::AFAbstractArray) = chol!(A,Val{:U})
 # Fourier Transforms
 # TODO: Multidimensional
 import Base: fft
-fft{T}(a::AFAbstractArray{T}) = AFArray{Complex{T}}(icxx"fft($a);")
-fft{T<:Complex}(a::AFAbstractArray{T}) = AFArray{T}(icxx"fft($a);")
+function fft{T}(a::AFAbstractArray{T})
+	if ndims(a) == 1
+		AFArray{Complex{T}}(icxx"fft($a);")
+	elseif ndims(a) == 2
+		AFArray{Complex{T}}(icxx"fft2($a);")
+	end
+end
+		
+function fft{T<:Complex}(a::AFAbstractArray{T})
+	if ndims(a) == 1
+		AFArray{T}(icxx"fft($a);")
+	elseif ndims(a) == 2
+		AFArray{T}(icxx"fft2($a);")
+	end
+end
 
 # Exception handling
 import Base: showerror
