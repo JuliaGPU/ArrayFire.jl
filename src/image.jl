2cd2941addf1910f32d39831779c2c1d65a983a5
ranjanan <benditlikeranjan@gmail.com>
2016-02-09 14:45:21 +0530

    Fix image processing functions

--- src/image.jl
+++ src/image.jl
@@ -1,23 +1,37 @@
 
 #Image
-function loadImage(str::String)
-	a = pointer(str)
-	return AFArray{Float32}(icxx"loadImage($a);")
+export loadImage, saveImage, rotate, scale, transform, skew, translate
+
+"Load image as an AFArray"
+function loadImage(path::AbstractString; color = false)
+	return AFArray{Float32}(icxx"loadImage($path, $color);")
 end
 
-function saveImage(str::String, a::AFArray{Float32})
-	b = pointer(str)
-	icxx"saveImage($b, $a);"
+"Save AFArray as image"
+function saveImage(path::AbstractString, a::AFAbstractArray)
+	icxx"saveImage($path, $a);"
 end
 
-function rotate(a::AFArray{Float32}, theta)
-	icxx"rotate($a, $theta);"
+"Rotate image by theta radians"
+function rotate{T}(a::AFAbstractArray{T}, theta)
+	AFArray{T}(icxx"rotate($a, $theta);")
 end
 
-function scale(a::AFArray{Float32}, scale1, scale2)
-	icxx"scale($a, $scale1, $scale2);"
+"Scale image in either dimension"
+function scale{T}(a::AFAbstractArray{T}, scale1::Real, scale2::Real)
+	AFArray{T}(icxx"scale($a, $scale1, $scale2);")
 end
 
-function transform(a::AFArray{Float32}, b::AFArray)
-	icxx"transform($a, $b);"
+"Skew an image in either dimension by angles"
+function skew{T}(a::AFAbstractArray{T}, skew1::Real, skew2::Real)
+	AFArray{T}(icxx"skew($a, $skew1, $skew2);")
+end
+
+"Translate a given image in either dimension"
+function translate{T}(a::AFAbstractArray{T}, trans1::Real, trans2::Real)
+	AFArray{T}(icxx"translate($a, $trans1, $trans2);")
+end
+
+function transform{T}(a::AFAbstractArray{T}, b::AFAbstractArray)
+	AFArray{T}(icxx"transform($a, $b);")
 end
