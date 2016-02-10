0d11c01ae2f031534dca7442c8db20eedbe7ca2e
ranjanan <benditlikeranjan@gmail.com>
2016-02-10 10:33:04 +0530

    Add mean of all element

--- src/stats.jl
+++ src/stats.jl
@@ -1,6 +1,8 @@
 import Base: mean
+export average
     
 #Mean
-#TODO : generate mean of all elements
+#TODO Import use Base.mean for this instead of a different name
+average{T}(a::AFAbstractArray{T}) = icxx"af::mean<float>($a);"
 mean{T}(a::AFAbstractArray{T}, dim::Integer) = AFArray{T}(icxx"af::mean($a, $(dim-1));")
 mean{T<:Real}(a::AFAbstractArray{T}, weights::AFAbstractArray{T}, dim::Integer) = AFArray(icxx"af::mean($a, $weights, $dim);")
