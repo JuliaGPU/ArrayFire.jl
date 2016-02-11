86e77f20f3bc9090c6fd3a0660e2b40dc0871511
ranjanan <benditlikeranjan@gmail.com>
2016-02-11 12:10:14 +0530

    Fix small dimensions bug

--- src/vector.jl
+++ src/vector.jl
@@ -5,36 +5,36 @@
 
 #Reduction operations
 
-import Base: sum, max, min, any 
+import Base: sum, maximum, min, any 
 export product, alltrue
 
 #Sum 
 sum{T}(a::AFAbstractArray{T}) = icxx"af::sum<float>($a);"
-sum{T}(a::AFAbstractArray{T}, dim::Integer) = AFArray{T}(icxx"af::sum($a, $(dim -1));")
+sum{T}(a::AFAbstractArray{T}, dim::Integer) = AFArray{T}(icxx"af::sum($a, $(dim - 1));")
 
 #Product
 product{T}(a::AFAbstractArray{T}) = icxx"af::product<float>($a);"
-product{T}(a::AFAbstractArray{T}, dim::Integer) = AFArray{T}(icxx"af::product($a, $(dim -1));")
+product{T}(a::AFAbstractArray{T}, dim::Integer) = AFArray{T}(icxx"af::product($a, $(dim - 1));")
 
 #Max
-max{T}(a::AFAbstractArray{T}) = icxx"af::max<float>($a);"
-max{T}(a::AFAbstractArray{T}, dim::Integer) = AFArray{T}(icxx"af::max($a, $(dim -1));")
+maximum{T}(a::AFAbstractArray{T}) = icxx"af::max<float>($a);"
+maximum{T}(a::AFAbstractArray{T}, dim::Integer) = AFArray{T}(icxx"af::max($a, $(dim - 1));")
 
 #Min
 min{T}(a::AFAbstractArray{T}) = icxx"af::min<float>($a);"
-min{T}(a::AFAbstractArray{T}, dim::Integer) = AFArray{T}(icxx"af::min($a, $(dim -1));")
+min{T}(a::AFAbstractArray{T}, dim::Integer) = AFArray{T}(icxx"af::min($a, $(dim - 1));")
 
 #Any
 any{T}(a::AFAbstractArray{T}) = icxx"af::anyTrue<boolean_t>($a);" != 0
-any(a::AFAbstractArray, dim::Integer) = AFArray{Bool}(icxx"af::anyTrue($a, $(dim -1));")
+any(a::AFAbstractArray, dim::Integer) = AFArray{Bool}(icxx"af::anyTrue($a, $(dim - 1));")
 
 #Alltrue
 alltrue{T}(a::AFAbstractArray{T}) = icxx"af::allTrue<boolean_t>($a);" != 0
-alltrue(a::AFAbstractArray, dim::Integer) = AFArray{Bool}(icxx"af::allTrue($a, $(dim -1));")
+alltrue(a::AFAbstractArray, dim::Integer) = AFArray{Bool}(icxx"af::allTrue($a, $(dim - 1));")
 
 #Count 
 countnz{T}(a::AFAbstractArray{T}) = icxx"af::count<float>($a);"
-countnz{T}(a::AFAbstractArray{T}, dim::Integer) = AFArray{UInt32}(icxx"af::count($a, $(dim -1));")
+countnz{T}(a::AFAbstractArray{T}, dim::Integer) = AFArray{UInt32}(icxx"af::count($a, $(dim - 1));")
 
 
 #Inclusive Scan Operations
