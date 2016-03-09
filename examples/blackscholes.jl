8a8203882f834004115a9d0aafd13d8a939e4ff4
ranjanan <benditlikeranjan@gmail.com>
2016-03-09 19:32:41 +0530

    Add credits for example

--- examples/blackscholes.jl
+++ examples/blackscholes.jl
@@ -1,3 +1,5 @@
+#This example has been adopted from https://github.com/IntelLabs/ParallelAccelerator.jl/blob/master/examples/black-scholes/black-scholes.jl
+
 function blackscholes_serial(sptprice::Array{Float64,1},
                            strike::Array{Float64,1},
                            rate::Array{Float64,1},
