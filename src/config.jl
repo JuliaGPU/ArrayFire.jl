e99051ad6881f4665c250c0dd672d15c22c21576
ranjanan <benditlikeranjan@gmail.com>
2016-08-31 23:31:36 +0530

    Fix try catch block

--- src/config.jl
+++ src/config.jl
@@ -1,13 +1,12 @@
 let
     global af_lib
-    succeeded = false
     if !isdefined(:af_lib)
         lib = is_unix()  ? begin "libaf" end : is_windows() ? "af" : ""
         try
             Libdl.dlopen(lib)
-            succeeded = true
+        catch
+            error("ArrayFire library not found")
         end
         @eval const af_lib = $lib
     end
-    succeeded || error("ArrayFire library not found")
 end
