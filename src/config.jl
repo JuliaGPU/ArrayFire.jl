db3301bb2e64b8a1590ca1db0a86067684c897a8
ranjanan <benditlikeranjan@gmail.com>
2016-08-31 23:20:56 +0530

    Fix the config file

--- src/config.jl
+++ src/config.jl
@@ -3,9 +3,11 @@ let
     succeeded = false
     if !isdefined(:af_lib)
         lib = is_unix()  ? begin "libaf" end : is_windows() ? "af" : ""
-        Libdl.dlopen(lib)
-        succeeded = true
-        succeeded || error("ArrayFire library not found")
+        try
+            Libdl.dlopen(lib)
+            succeeded = true
+        end
         @eval const af_lib = $lib
     end
+    succeeded || error("ArrayFire library not found")
 end
