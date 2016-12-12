899e26646ae711f9f6bfbcca0369e185f55eade6
Joe Petviashvili <joe@gnom.us>
2016-12-12 15:47:39 -0800

    more tests

--- test/autodiff.jl
+++ test/autodiff.jl
@@ -18,7 +18,19 @@ end
 
 typealias AFVector AFArray{Float64,1}
 typealias AFMatrix AFArray{Float64,2}
+
+import AutoDiffSource: δsum, δdot_power, δdot_power_const1, δdot_power_const2
+
 δsum(x::AFArray) = (t = size(x); (sum(x), z->constant(z, t)))
+δdot_power(x::AFArray, y::AbstractFloat) = (t = x.^y; (t, z->(z.*y.*t ./ x, sum(z.*t.*log(x)))))
+δdot_power(x::AbstractFloat, y::AFArray) = (t = x.^y; (t, z->(sum(z.*y.*t)./x, z.*t.*log(x))))
+δdot_power(x::AFMatrix, y::AFVector) = (t = x.^y; (t, z->(z.*y.*t./x, vec(sum(z.*t.*log(x), 2)))))
+δdot_power(x::AFVector, y::AFMatrix) = (t = x.^y; (t, z->(vec(sum(z.*y.*t./x, 2)), z.*t.*log(x))))
+δdot_power_const1(x, y::AFArray) = (t = x.^y; (t, z->z.*t.*log(x)))
+δdot_power_const2(x::AFArray, y) = (t = x.^y; (t, z-> y == 2 ? z.*2x : z.*y.*t./x))
+δdot_power(x::AFArray, y::AFArray) = (t = x.^y; (t, z->(z.*y.*t./x, z.*t.*log(x))))
+
+
 
 rnd() = rand(AFVector, 1)
 rnd(len) = rand(AFVector, len)
@@ -39,4 +51,36 @@ for o in [:+, :-, :*]
     δt = Symbol("δ$t")
     @eval @δ $t(x) = sum($o(2., x))
     @test @eval checkdiff_inferred($t, $δt, rnd())
+end
+
+# (vector, vector), (matrix, matrix), (const, *), (*, const), (const, const)
+# (vector, matrix), (matrix, vector), (vector, scalar), (matrix, scalar), (scalar, vector), (scalar, matrix)
+for o in [:.+, :.-, :.*, :./, :.^]
+    t = gensym(o)
+    δt = Symbol("δ$t")
+    @eval @δ $t(x, y) = sum($o(x, y))
+    @eval @test checkdiff_inferred($t, $δt, rnd(5), rnd(5))
+    @eval @test checkdiff_inferred($t, $δt, rnd(3, 2), rnd(3, 2))
+
+    t = gensym(o)
+    δt = Symbol("δ$t")
+    @eval @δ $t(x) = sum($o(x, 3.))
+    @eval @test checkdiff_inferred($t, $δt, rnd(5))
+    @eval @test checkdiff_inferred($t, $δt, rnd(3, 2))
+
+    t = gensym(o)
+    δt = Symbol("δ$t")
+    @eval @δ $t(x) = sum($o(3., x))
+    @eval @test checkdiff_inferred($t, $δt, rnd(5))
+    @eval @test checkdiff_inferred($t, $δt, rnd(3, 2))
+
+    t = gensym(o)
+    δt = Symbol("δ$t")
+    @eval @δ $t(x, y) = sum($o(x, y))
+    @eval @test checkdiff_inferred($t, $δt, rnd(3), rnd(3, 2))
+    @eval @test checkdiff_inferred($t, $δt, rnd(3, 2), rnd(3))
+    @eval @test checkdiff_inferred($t, $δt, rnd(5), rand())
+    @eval @test checkdiff_inferred($t, $δt, rnd(3, 2), rand())
+    @eval @test checkdiff_inferred($t, $δt, rand(), rnd(5))
+    @eval @test checkdiff_inferred($t, $δt, rand(), rnd(3, 2))
 end
