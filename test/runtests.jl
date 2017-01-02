1e1ec4327df970acfa7a923e282eb71611b78c55
Joe Petviashvili <joe@gnom.us>
2017-01-02 14:00:59 -0800

    test for success

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using ArrayFire
 using Base.Test
 
 @test af_info() == nothing
-@test isa(af_err_to_string(Cuint(0)), Cstring)
+@test unsafe_string(af_err_to_string(Cuint(0))) == "Success"
 @test_throws ErrorException af_set_device(Cint(-5))
