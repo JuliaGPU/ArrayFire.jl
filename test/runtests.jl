82a0523c88d40eba1df9e356fcd5da5b0334b44c
Joe Petviashvili <joe@gnom.us>
2017-01-03 20:03:47 -0800

    easier to convert types

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,3 +18,8 @@ arr4 = @inferred Array{Int,1}(arr1)
 @test arr4 == [1, 2]
 arr5 = AFArray{Int,2}([1 2 3; 4 5 6])
 @test (@inferred size(arr5)) == (2,3)
+arr6 = @inferred AFArray([1, 2])
+arr7 = @inferred Array(arr6)
+@test eltype(arr6) == eltype(arr7)
+@test ndims(arr6) == ndims(arr7)
+@test size(arr6) == size(arr7)
