10481694cb350b7ea7a720198388d9ddd8b1e94b
Joe Petviashvili <joe@gnom.us>
2017-01-31 23:23:29 -0800

    more types

--- test/runtests.jl
+++ test/runtests.jl
@@ -129,6 +129,18 @@ a4 = rand(UInt8, 2, 3)
 af4 = AFArray(a4)
 c1 = rand(UInt8)
 
+for op in [:&, :|, :xor]
+    @test @eval all($op.(a3, a4) .== Array(@inferred $op(af3, af4)))
+    @test @eval all($op.(c1, a4) .== Array(@inferred $op(c1, af4)))
+    @test @eval all($op.(a3, c1) .== Array(@inferred $op(af3, c1)))
+end
+
+a3 = rand(UInt32, 2, 3)
+af3 = AFArray(a3)
+a4 = rand(UInt32, 2, 3)
+af4 = AFArray(a4)
+c1 = rand(UInt32)
+
 for op in [:<<, :>>, :&, :|, :xor]
     @test @eval all($op.(a3, a4) .== Array(@inferred $op(af3, af4)))
     @test @eval all($op.(c1, a4) .== Array(@inferred $op(c1, af4)))
