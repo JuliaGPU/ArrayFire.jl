b9629f9a161d5c232a13bfc2ba33b1031bb1a327
Joe Petviashvili <joe@gnom.us>
2017-07-21 13:24:09 +0300

    more tests

--- test/fft.jl
+++ test/fft.jl
@@ -4,6 +4,8 @@ for T in (Float32, Float64)
     af1 = AFArray(a1)
     bf1 = fft(af1, 1., 0)
     cf1 = ifft(bf1, 1 / length(bf1), 0)
+    @test isa(bf1, AFArray)
+    @test isa(cf1, AFArray)
     @test eltype(bf1) == eltype(cf1) == Complex{T}
     @test Array(bf1) ≈ b1
     @test Array(cf1) ≈ a1
@@ -22,6 +24,8 @@ for T in (Float32, Float64)
     af2 = AFArray(a2)
     bf2 = fft2(af2, 1., 0, 0)
     cf2 = ifft2(bf2, 1 / length(bf2), 0, 0)
+    @test isa(bf2, AFArray)
+    @test isa(cf2, AFArray)
     @test eltype(bf2) == eltype(cf2) == Complex{T}
     @test Array(bf2) ≈ b2
     @test Array(cf2) ≈ a2
@@ -40,6 +44,8 @@ for T in (Float32, Float64)
     af3 = AFArray(a3)
     bf3 = fft3(af3, 1., 0, 0, 0)
     cf3 = ifft3(bf3, 1 / length(bf3), 0, 0, 0)
+    @test isa(bf3, AFArray)
+    @test isa(cf3, AFArray)
     @test eltype(bf3) == eltype(cf3) == Complex{T}
     @test Array(bf3) ≈ b3
     @test Array(cf3) ≈ a3
@@ -58,6 +64,8 @@ for T in (Float32, Float64)
     arf1 = AFArray(ar1)
     brf1 = fft_r2c(arf1, 1., 0)
     crf1 = fft_c2r(brf1, 1 / length(arf1), isodd(size(arf1, 1)))
+    @test isa(brf1, AFArray)
+    @test isa(crf1, AFArray)
     @test eltype(brf1) == Complex{T}
     @test eltype(crf1) == T
     @test Array(brf1) ≈ br1
@@ -68,6 +76,10 @@ for T in (Float32, Float64)
     arf2 = AFArray(ar2)
     brf2 = fft2_r2c(arf2, 1., 0, 0)
     crf2 = fft2_c2r(brf2, 1 / length(arf2), isodd(size(arf2, 1)))
+    @test isa(brf2, AFArray)
+    @test isa(crf2, AFArray)
+    @test isa(bf1, AFArray)
+    @test isa(cf1, AFArray)
     @test eltype(brf2) == Complex{T}
     @test eltype(crf2) == T
     @test Array(brf2) ≈ rfft(ar2)
@@ -78,6 +90,8 @@ for T in (Float32, Float64)
     arf3 = AFArray(ar3)
     brf3 = fft3_r2c(arf3, 1., 0, 0, 0)
     crf3 = fft3_c2r(brf3, 1 / length(arf3), isodd(size(arf3, 1)))
+    @test isa(brf3, AFArray)
+    @test isa(crf3, AFArray)
     @test eltype(brf3) == Complex{T}
     @test eltype(crf3) == T
     @test Array(brf3) ≈ rfft(ar3)
