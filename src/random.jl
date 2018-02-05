55acfb97c8809fbc1a927a6ebfd5608143322b08
Joe Petviashvili <joe@gnom.us>
2018-02-04 20:34:33 -0800

    fix warnings

--- src/random.jl
+++ src/random.jl
@@ -6,9 +6,9 @@ function rand(::Type{AFArray{T,N}}, t::NTuple{N,Int}) where {T,N}
                  out,UInt32(N),[t...],af_type(T)))
     AFArray{T,N}(out[])
 end
-rand{T,N}(::Type{AFArray{T}}, t::NTuple{N,Int}) = rand(AFArray{T,N}, t)
-rand{N}(::Type{AFArray}, t::NTuple{N,Int}) = rand(AFArray{Float32,N}, t)
-rand{T}(::Type{AFArray{T}}, t::Int...) = rand(AFArray{T}, t)
+rand(::Type{AFArray{T}}, t::NTuple{N,Int}) where {T,N}= rand(AFArray{T,N}, t)
+rand(::Type{AFArray}, t::NTuple{N,Int}) where {N} = rand(AFArray{Float32,N}, t)
+rand(::Type{AFArray{T}}, t::Int...) where {T} = rand(AFArray{T}, t)
 rand(::Type{AFArray}, t::Int...) = rand(AFArray, t)
 
 function randn(::Type{AFArray{T,N}}, t::NTuple{N,Int}) where {T,N}
@@ -17,9 +17,9 @@ function randn(::Type{AFArray{T,N}}, t::NTuple{N,Int}) where {T,N}
                  out,UInt32(N),[t...],af_type(T)))
     AFArray{T,N}(out[])
 end
-randn{T,N}(::Type{AFArray{T}}, t::NTuple{N,Int}) = randn(AFArray{T,N}, t)
-randn{N}(::Type{AFArray}, t::NTuple{N,Int}) = randn(AFArray{Float32,N}, t)
-randn{T}(::Type{AFArray{T}}, t::Int...) = randn(AFArray{T}, t)
+randn(::Type{AFArray{T}}, t::NTuple{N,Int}) where {T,N} = randn(AFArray{T,N}, t)
+randn(::Type{AFArray}, t::NTuple{N,Int}) where {N} = randn(AFArray{Float32,N}, t)
+randn(::Type{AFArray{T}}, t::Int...) where {T} = randn(AFArray{T}, t)
 randn(::Type{AFArray}, t::Int...) = randn(AFArray, t)
 
 srand(::Type{AFArray}, i) = set_seed(UInt(abs(i)))
