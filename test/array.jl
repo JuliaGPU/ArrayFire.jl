7a26ffc22808c99556c58bae5d674600477e1698
Joe Petviashvili <joe@gnom.us>
2018-08-09 12:23:08 -0700

    fix range

--- test/array.jl
+++ test/array.jl
@@ -1,13 +1,13 @@
 @testset "Array" begin
     @testset "range" begin
         x = range(AFArray{Float32}, 1, 5)
-        @test Array(x) == Array{Float32}(collect(range(1, 5)))
+        @test Array(x) == Array{Float32}(collect(1.0:5.0))
         @test typeof(x) == AFArray{Float32, 1}
         x = range(AFArray{Float32}, 3, 5)
-        @test Array(x) == Array{Float32}(collect(range(3, 5)))
+        @test Array(x) == Array{Float32}(collect(3.0:7.0))
         @test typeof(x) == AFArray{Float32, 1}
         x = range(AFArray{Float32}, 2, 3, 10)
-        @test Array(x) == collect(range(2,3,10))
+        @test Array(x) == collect(2.0:3:29.0)
         @test typeof(x) == AFArray{Float32, 1}
 
         @test sum(@inferred iota((2,3))) == 21
